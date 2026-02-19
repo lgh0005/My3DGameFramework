3006b8f1661fad04afcf3422312b40828781357f
lgh0005 <lgh0005@naver.com>
2026-02-19 17:08:23 +0900

    Engine 폴더명을 My3DGameFramework로 수정

---
+++ My3DGameFramework/Core/CorePch.h
@@ -0,0 +1,51 @@
+﻿#pragma once
+
+/*============================//
+//   C++ standard libraries   //
+//============================*/
+// C standard library wrappers
+#include <cstdint>
+#include <climits>
+#include <cmath>
+#include <cstdarg>
+
+// C++ strings
+#include <string>
+#include <string_view>
+
+// C++ containers
+#include <vector>
+#include <array>
+#include <unordered_map>
+
+// C++ memories
+#include <memory>
+#include <memory_resource>
+namespace pmr = std::pmr;
+
+// C++ templates
+#include <type_traits>
+
+// C++ thread
+#include <thread>
+#include <mutex>
+
+// C++ utilities
+#include <random>
+#include <optional>
+#include <algorithm>
+#include <functional>
+
+// C++ filesystem
+#include <filesystem>
+#include <fstream>
+#include <sstream>
+namespace fs = std::filesystem;
+
+/*===========================//
+//   Engine common headers   //
+//===========================*/
+#include "Common/Defines.h"
+#include "Common/Enums.h"
+#include "Common/Types.h"
+#include "Common/Constants.h"
