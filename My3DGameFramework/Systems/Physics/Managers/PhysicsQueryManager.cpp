b9521d3436823fd331c688fb848328380d02ba08
lgh0005 <lgh0005@naver.com>
2026-03-16 22:17:59 +0900

    폴더명 변경 및 쿼리 처리 매니저 이름 변경

--- My3DGameFramework/Systems/Physics/Managers/PhysicsQueryManager.cpp
+++ My3DGameFramework/Systems/Physics/Managers/PhysicsQueryManager.cpp
@@ -1,10 +1,22 @@
 #include "PhysicsPch.h"
 #include "PhysicsQueryManager.h"
+#include "Managers/PhysicsManager.h"
 
 namespace MGF3D
 {
 	PhysicsQueryManager::PhysicsQueryManager() = default;
 	PhysicsQueryManager::~PhysicsQueryManager() = default;
 
+	void PhysicsQueryManager::Init()
+	{
+		m_querySystem = &PhysicsManager::Instance().GetNarrowPhaseQuery();
+		MGF_ASSERT(m_querySystem != nullptr, "Failed to hook NarrowPhaseQuery! PhysicsManager must be initialized BEFORE PhysicsQueryManager.");
+		MGF_LOG_INFO("[Physics] PhysicsQueryManager Initialized (NarrowPhaseQuery Hooked).");
+	}
 
+	void PhysicsQueryManager::Shutdown()
+	{
+		m_querySystem = nullptr;
+		MGF_LOG_INFO("[Physics] PhysicsQueryManager Shutdown.");
+	}
 }
--- My3DGameFramework/Systems/Physics/Managers/PhysicsQueryManager.h
+++ My3DGameFramework/Systems/Physics/Managers/PhysicsQueryManager.h
@@ -11,9 +11,18 @@ namespace MGF3D
 		~PhysicsQueryManager();
 
 	public:
+		void Init();
+		void Shutdown();
+
+	public:
+		/*=================================//
+		//   예약 실행형 (비동기) API 뼈대   //
+		//=================================*/
+		// INFO : 여기에서는 우리가 구축해둔 TaskManager를 이용해봄직 함
+		// void EnqueueQuery(UniquePtr<IPhysicsQuery> query);
+		// ...
 
 	private:
-
-
+		Ptr<const ::JPH::NarrowPhaseQuery> m_querySystem{ nullptr };
 	};
 }
